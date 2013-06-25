22bc35f9b2f551472246031797c07fd60156650f
wysiwyng <wysiwyng@turbine.local>
2013-06-25 15:12:09 +0200

    testing enqueueing of new commands

--- operatorClient/OpClient.cs
+++ operatorClient/OpClient.cs
@@ -33,16 +33,7 @@ namespace operatorClient
 
             op = new Operator(0, UID, DateTime.Now, 0);     //create new operator object for the serverController to work with
 
-            serverController = new ServerController(op);
-
-            if (serverController.getUri() == null || serverController.getUri() == "")         //load and check server controller uri, if not set prompt for it
-            {
-                Console.Write("server uri>");
-                serverController.saveUri(Console.ReadLine());
-                serverController.setUri(serverController.loadUri());
-            }
-
-            serverController.register();                    //register the uid
+            commandController = new CommandController(op);
 
             String input = "";
 
@@ -52,50 +43,13 @@ namespace operatorClient
                 input = Console.ReadLine();                 //and read it
                 switch (input)                              //and checks what you entered
                 {
-                    case "test":
-                        Console.WriteLine(Encoder.stringToHex("\f"));
-                        Console.WriteLine(Encoder.stringToHex("none"));
-                        break;
-                    case "getTargets":
-                        Target[] targets = serverController.getTargets();
-                        foreach (Target target in targets)
-                        {
-                            Console.WriteLine(target.ToString());
-                        }
-                        break;
                     case "cmd":
                         Console.Write("to>");
-                        String to = Console.ReadLine();
+                        int toUID = Convert.ToInt32(Console.ReadLine());
                         Console.Write("cmd>");
-                        String data = Console.ReadLine();
-                        Console.WriteLine(serverController.saveCommand(new Command(Convert.ToInt32(to), data)).ToString()); //creates a new command, saves it on the server and displays it again in the prompt
+                        String cmdData = Console.ReadLine();
+                        commandController.saveCommand(new Command(toUID, cmdData));
                         break;
-                    case "listCmds":
-                        Command[] commands = serverController.listCommands();
-                        foreach (Command command in commands)
-                        {
-                            Console.WriteLine(command.ToString());
-                        }
-                        break;
-                    case "getCmd":
-                        Console.Write("id>");
-                        String id = Console.ReadLine();
-						Command cmd = serverController.getCommand(Convert.ToInt32 (id));
-						Console.WriteLine(cmd != null ? cmd.ToString() : "command not foud");
-                        break;
-                    case "resp":
-                        Console.Write("cmd ID>");
-                        String cmdID = Console.ReadLine();
-                        Console.Write("response>");
-                        String response = Console.ReadLine();
-                        Console.WriteLine(serverController.saveResponse(new Response(Convert.ToInt32(cmdID), Convert.ToInt32(UID), response)).ToString());
-                        break;
-					case "getResp":
-						Console.Write("resp ID>");
-						String respID = Console.ReadLine ();
-						Response resp = serverController.getResponseById(Convert.ToInt32(respID));
-						Console.WriteLine(resp != null ? resp.ToString() : "response not foud");
-						break;
                     default:
                         Console.WriteLine("unsupported command");
                         break;
