d18946184e2bcd3e98e9c1d34bec498ac9299842
wysiwyng <wysiwyng@turbine.local>
2013-06-27 14:47:44 +0200

    setting command to read when fetched from server

--- serverBackend/index.php
+++ serverBackend/index.php
@@ -2,8 +2,8 @@
 
 include 'config.inc.php';
 
-function getCommandById($id) {
-    $sql_reply = mysql_query("SELECT * FROM `commands` WHERE `ID`='" . $id . "'") or die("error fetching command, mysql said: " . mysql_error());
+function getCommandById($id, $uid) {
+    $sql_reply = mysql_query("SELECT * FROM `commands` WHERE `ID`='" . $id . "'") or die("!error fetching command, mysql said: " . mysql_error());
     if(mysql_num_rows($sql_reply) == 0) {
         die("!command not found");
     }
@@ -17,10 +17,13 @@ function getCommandById($id) {
     echo $row['read'];
     echo " C ";
     echo $row['cmd'];
+	if($row['to'] == $uid) {
+		mysql_query("UPDATE `commands` SET `read` = '74 72 75 65' WHERE `ID` = '" . $id . "'") or die("!error updating command, mysql said: " . mysql_error());
+	}
 }
 
 function getResponseById($id) {
-    $sql_reply = mysql_query("SELECT * FROM `responses` WHERE `ID`='" . $id . "'") or die("error fetching command, mysql said: " . mysql_error());
+    $sql_reply = mysql_query("SELECT * FROM `responses` WHERE `ID`='" . $id . "'") or die("!error fetching command, mysql said: " . mysql_error());
     if(mysql_num_rows($sql_reply) == 0) {
         die("!response not found");
     }
@@ -37,7 +40,7 @@ function getResponseById($id) {
 }
 
 function getResponseByCmdId($id) {
-    $sql_reply = mysql_query("SELECT * FROM `responses` WHERE `cmdId`='" . $id . "'") or die("error fetching command, mysql said: " . mysql_error());
+    $sql_reply = mysql_query("SELECT * FROM `responses` WHERE `cmdId`='" . $id . "'") or die("!error fetching command, mysql said: " . mysql_error());
     if(mysql_num_rows($sql_reply) == 0) {
         die("!response not found");
     }
@@ -57,7 +60,7 @@ if($_POST) {
     mysql_connect("localhost","$mysql_user","$mysql_pass") or die(mysql_error());
     mysql_select_db("$mysql_db") or die(mysql_error());
 
-    $sql_reply = mysql_query("SELECT * FROM `users` WHERE `UID` = '" . $_POST['UID'] . "'") or die("%failed to query users, mysql error was: " . mysql_error());
+    $sql_reply = mysql_query("SELECT * FROM `users` WHERE `UID` = '" . $_POST['UID'] . "'") or die("!failed to query users, mysql error was: " . mysql_error());
     $user_exists = false;
     if(mysql_num_rows($sql_reply) > 0) {
         $user_exists = true;
@@ -68,7 +71,7 @@ if($_POST) {
     switch($_POST['action']) {        
         case "register":
             if(!$user_exists) {
-                mysql_query("INSERT INTO `users` (`ID`,`UID`,`type`,`lastOnlineTime`) VALUES (NULL,'" . $_POST['UID'] . "','" . $_POST['type'] . "','" . $_POST['timestamp'] . "');") or die("%failed to insert new operator, mysql error was: " . mysql_error());
+                mysql_query("INSERT INTO `users` (`ID`,`UID`,`type`,`lastOnlineTime`) VALUES (NULL,'" . $_POST['UID'] . "','" . $_POST['type'] . "','" . $_POST['timestamp'] . "');") or die("!failed to insert new operator, mysql error was: " . mysql_error());
                 echo "%inserted new user\n";
             }
             else {
@@ -76,7 +79,7 @@ if($_POST) {
             }
             break;
         case "getTargets":
-            $sql_reply = mysql_query("SELECT * FROM `users` WHERE `type` = '74 61 72 67 65 74'");
+            $sql_reply = mysql_query("SELECT * FROM `users` WHERE `type` = '74 61 72 67 65 74'") or die("!failed to get targets, mysql said: " . mysql_error());
             if(mysql_num_rows($sql_reply) == 0) {
                 die("%no targets...\n");
             }
@@ -90,19 +93,20 @@ if($_POST) {
             }
             break;
         case "saveCommand":
-            $sql_reply = mysql_query("INSERT INTO `commands` (`ID`,`from`,`to`,`read`,`cmd`) VALUES (NULL,'" . $_POST['UID'] . "','" . $_POST['to'] . "','66 61 6C 73 65','" . $_POST['cmd'] . "');") or die ("%error inserting new command, mysql said: " . mysql_error());
-            getCommandById(mysql_insert_id());
+            $sql_reply = mysql_query("INSERT INTO `commands` (`ID`,`from`,`to`,`read`,`cmd`) VALUES (NULL,'" . $_POST['UID'] . "','" . $_POST['to'] . "','66 61 6C 73 65','" . $_POST['cmd'] . "');") or die ("!error inserting new command, mysql said: " . mysql_error());
+            getCommandById(mysql_insert_id(), $_POST['UID']);
             break;
         case "saveResponse":
-            mysql_query("INSERT INTO `responses` (`ID`,`cmdId`,`UID`,`read`,`response`) VALUES (NULL,'" . $_POST['cmdId'] . "','" . $_POST['UID'] . "','66 61 6C 73 65','" . $_POST['response'] . "');") or die ("%error inserting new response, mysql said: " . mysql_error());           
+            mysql_query("INSERT INTO `responses` (`ID`,`cmdId`,`UID`,`read`,`response`) VALUES (NULL,'" . $_POST['cmdId'] . "','" . $_POST['UID'] . "','66 61 6C 73 65','" . $_POST['response'] . "');") or die ("!error inserting new response, mysql said: " . mysql_error());           
             getResponseById(mysql_insert_id());
             break;
         case "listCommands":
-            $sql_reply = mysql_query("SELECT * FROM `commands` WHERE `to`='" . $_POST['UID'] . "'") or die("%error fetching commands, mysql said: " . mysql_error());
+            $sql_reply = mysql_query("SELECT * FROM `commands` WHERE `to`='" . $_POST['UID'] . "'") or die("!error fetching commands, mysql said: " . mysql_error());
             if(mysql_num_rows($sql_reply) == 0) {
                 die("%no commands\n");
             }
             while($row = mysql_fetch_array($sql_reply)) {
+				if($row['read'] == "74 72 75 65") continue;
                 echo $row['ID'];
                 echo "\r";
                 echo $row['from'];
@@ -114,7 +118,7 @@ if($_POST) {
             }
             break;
         case "getCommand":
-            getCommandById($_POST['ID']);
+            getCommandById($_POST['ID'], $_POST['UID']);
             break;
         case "getResponseById":
 		    getResponseById($_POST['ID']);
