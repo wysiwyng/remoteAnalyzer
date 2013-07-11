c0157de527a74f7371b6c850a01f5fe2bae46ec3
wysiwyng <fuer_muell@nurfuerspam.de>
2013-07-11 11:20:18 +0200

    Create CommandInterpreter.cs

---
+++ targetClient/CommandInterpreter.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace targetClient
+{
+    class CommandInterpreter
+    {
+        public CommandInterpreter()
+        {
+        
+        }
+    }
+}
